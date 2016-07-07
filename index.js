2153734e146fdc1751b57379d320b4b934258833
dineshgouttam <dinesh.gouttam@decurtis.com>
2016-07-07 13:30:31 +0530

    First Code Commit

---
+++ index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const restService = express();
+restService.use(bodyParser.json());
+
+restService.post('/hook', function (req, res) {
+
+    console.log('hook request');
+
+    try {
+        var speech = 'empty speech';
+
+        if (req.body) {
+            var requestBody = req.body;
+
+            if (requestBody.result) {
+                speech = '';
+
+                if (requestBody.result.fulfillment) {
+                    speech += requestBody.result.fulfillment.speech;
+                    speech += ' ';
+                }
+
+                if (requestBody.result.action) {
+                    if(requestBody.result.action == "getTotalCost")
+                    {
+                        var num1 = parseInt(requestBody.result.parameters.number1);
+                        var num2 = parseInt(requestBody.result.parameters.number2);
+                        var cost = num1 + num2;
+                        speech += 'totalCost: ' + cost;
+                    }
+                    speech += 'action: ' + requestBody.result.action;
+                }
+            }
+        }
+
+        console.log('result: ', speech);
+
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'apiai-webhook-sample'
+        });
+    } catch (err) {
+        console.error("Can't process request", err);
+
+        return res.status(400).json({
+            status: {
+                code: 400,
+                errorType: err.message
+            }
+        });
+    }
+});
+
+restService.listen((process.env.PORT || 5000), function () {
+    console.log("Server listening");
+});
